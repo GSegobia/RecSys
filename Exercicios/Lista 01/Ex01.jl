7635760767a8f92f585db019f5ac2a3facecb8b9
GSegobia <segobia.gos@gmail.com>
2016-10-01 20:52:13 -0300

    Correções Lista 01

--- Exercicios/Lista 01/Ex01.jl
+++ Exercicios/Lista 01/Ex01.jl
@@ -2,46 +2,22 @@ using PyPlot
 
 file_directory = "../../Listas/ml-100k/u.data"
 
-columns_number = 4
-
 f = open(file_directory)
 
 file_content = readdlm(f)
 
 close(f)
 
-file_content = sortrows(file_content, by = x->(x[1]))
+#file_content = sortrows(file_content, by = x->(x[1]))
+file_content = sort(file_content[:, 1])
 
-println(hist(file_content[:, 1], convert(Int64, file_content[end, 1])))
+rates_by_user = sort(hist(file_content, convert(Int64, file_content[end]))[2])
 
-# occurrences_user = []
-# occurrences_times = []
-# count = 0
-#
-# curr_number = 0
-# prev_number = file_content[1,1]
-#
-# for i = 1:size(file_content)[1]
-#     curr_number = file_content[i, 1]
-#
-#     if  curr_number == prev_number
-#         count = count + 1
-#     else
-#         occurrences_user = append!(occurrences_user, [prev_number])
-#         occurrences_times = append!(occurrences_times, [count])
-#         count = 1
-#     end
-#
-#     prev_number = curr_number
-# end
-#
-# ccurrences_user = append!(occurrences_user, [prev_number])
-# occurrences_times = append!(occurrences_times, [count])
-#
-# occurrences = append!(occurrences_user, occurrences_times)
-# occurrences = reshape(occurrences, convert(Int64, length(occurrences)/2), 2)
-#
-# occurrences = sortrows(occurrences, by = x->(x[2]))
-#
-# plot(1:size(occurrences,1),occurrences[:,2])
-# show()
+plot(1:convert(Int64, file_content[end]), rates_by_user)
+
+grid("on")
+xlabel("Quantidade de Notas")
+ylabel("Número de Usuários")
+title("Notas")
+
+show()
--- Exercicios/Lista 01/Ex02.jl
+++ Exercicios/Lista 01/Ex02.jl
@@ -1,8 +1,6 @@
 using PyPlot
 
 file_directory = "../../Listas/ml-100k/u.data"
-notas = 5
-columns_number = 4
 
 f = open(file_directory)
 
@@ -10,10 +8,16 @@ file_content = readdlm(f)
 
 close(f)
 
-file_content = sortrows(file_content, by = x->(x[3]))
+#file_content = sortrows(file_content, by = x->(x[3]))
+file_content = sort(file_content[:, 3])
+
+rates_number = sort(hist(file_content, convert(Int64, file_content[end]))[2])
 
 figure("Histograma")
-plt[:hist](file_content[:, 3], 1:6)
+
+# plt[:hist](file_content[:, 3], 1:6)
+bar(1:length(rates_number), rates_number)
+
 grid("on")
 xlabel("Notas")
 ylabel("Usuários")
